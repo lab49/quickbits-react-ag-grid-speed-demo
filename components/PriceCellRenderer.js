cf3a39002289c7d3a8c472bc207d2363fa1774c7
CollinMonahanLab49 <collin.monahan@lab49.com>
2021-08-23 14:04:51 -0400

    don't repeat $ on every cell

--- components/PriceCellRenderer.js
+++ components/PriceCellRenderer.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Flash } from '@lab49/react-value-flash'
 
 import { DOWN_COLOR, UP_COLOR } from '../lib/colors'
+import { formatPrice } from '../lib/formatters'
 
 const PriceCellRenderer = props => {
   const { value } = props
   return (
     <Flash
       value={value}
-      formatter="currency"
+      formatterFn={formatPrice}
       downColor={DOWN_COLOR}
       upColor={UP_COLOR}
       timeout={75}
